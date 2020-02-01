4b7d41748d97ed5530cba2e77769615e5ced8862
Rufino <rufinoansara@gmail.com>
2020-02-01 00:53:16 -0500

    Joystick moves

--- Assets/Scripts/Enums.cs
+++ Assets/Scripts/Enums.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class Enums 
 {
@@ -23,5 +24,29 @@ public class Enums
         "Y_",
         "LB_",
         "RB_"
+    };
+
+    public static string[] RepairStickDirections =
+    {
+        "Up",
+        "Down",
+        "Left",
+        "Right",
+        "Up/Left",
+        "Up/Right",
+        "Down/Left",
+        "Down/Right"
+    };
+
+    public static Dictionary<string, float[]> StickRepairDirectionValues = new Dictionary<string, float[]>()
+    {
+        {"Up", new float[]{0, -1} },
+        {"Down", new float[]{0, 1} },
+        {"Left", new float[]{-1, 0} },
+        {"Right", new float[]{1, 0} },
+        {"Up/Left", new float[]{-1, -1} },
+        {"Up/Right", new float[]{1, -1} },
+        {"Down/Left", new float[]{-1, 1} },
+        {"Down/Right", new float[]{1, 1} }
     };
 }
--- Assets/Scripts/Repair.cs
+++ Assets/Scripts/Repair.cs
@@ -11,6 +11,8 @@ public class Repair : MonoBehaviour
     private bool _isComplete = false;
     public bool IsComplete { get { return _isComplete; } }
 
+    private Enums.RepairType _repairType;
+
     public Dictionary<string, Enums.RepairType> repairRequirements = new Dictionary<string, Enums.RepairType>();
 
     public Repair()
@@ -22,10 +24,23 @@ public class Repair : MonoBehaviour
     {
         foreach (KeyValuePair<string, Enums.RepairType> keyValuePair in repairRequirements)
         {
-            if (!Input.GetButtonDown(keyValuePair.Key + controller.controllerId))
+            switch(keyValuePair.Value)
             {
-                _isComplete =  false;
-                return _isComplete;
+                case Enums.RepairType.StickDirection:
+                    if ((Input.GetAxis(controller.joyRightHori) - Enums.StickRepairDirectionValues[keyValuePair.Key][0]) > 0.1f &&
+                        (Input.GetAxis(controller.joyRightVert) - Enums.StickRepairDirectionValues[keyValuePair.Key][1]) > 0.1f)
+                    {
+                        _isComplete = false;
+                        return _isComplete;
+                    }
+                    break;
+                default:
+                    if (!Input.GetButtonDown(keyValuePair.Key + controller.controllerId))
+                    {
+                        _isComplete = false;
+                        return _isComplete;
+                    }
+                    break;
             }
         }
 
@@ -37,14 +52,16 @@ public class Repair : MonoBehaviour
     {
         int typeOfRepair = Random.Range(0, 10);
 
-        if(typeOfRepair < 5)
+        if(typeOfRepair < 8)
         {
             //button press
-            repairRequirements.Add(Enums.RepairButtons[(int)Random.Range(0, Enums.RepairButtons.Length)], Enums.RepairType.ButtonPress);
+            _repairType = Enums.RepairType.ButtonPress;
+            repairRequirements.Add(Enums.RepairButtons[(int)Random.Range(0, Enums.RepairButtons.Length)], _repairType);
         }
-        else
+        else if(typeOfRepair < 11)
         {
             // button combo press
+            _repairType = Enums.RepairType.ComboButtonPress;
             string firstButton = Enums.RepairButtons[(int)Random.Range(0, Enums.RepairButtons.Length)];
             string secondButton = Enums.RepairButtons[(int)Random.Range(0, Enums.RepairButtons.Length)];
 
@@ -53,8 +70,13 @@ public class Repair : MonoBehaviour
                 secondButton = Enums.RepairButtons[(int)Random.Range(0, Enums.RepairButtons.Length)];
             }
 
-            repairRequirements.Add(firstButton, Enums.RepairType.ComboButtonPress);
-            repairRequirements.Add(secondButton, Enums.RepairType.ComboButtonPress);
+            repairRequirements.Add(firstButton, _repairType);
+            repairRequirements.Add(secondButton, _repairType);
+        }
+        else
+        {
+            _repairType = Enums.RepairType.StickDirection;
+            repairRequirements.Add(Enums.RepairStickDirections[(int)Random.Range(0, Enums.RepairStickDirections.Length)], _repairType);
         }
 
         foreach(KeyValuePair<string, Enums.RepairType> keyValuePair in repairRequirements)
