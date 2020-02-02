494c92af39ad7d4a8ff81f895c96c58ecb702eee
Quang <quwanger@gmail.com>
2020-02-02 00:56:43 -0500

    Working on the main menu animation into

---
+++ Assets/PressStartUI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressStartUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void StartCircleAnim()
+    {
+        GameObject.FindGameObjectWithTag("PressStartCircle").GetComponent<SpriteRenderer>().enabled = true;
+        GameObject.FindGameObjectWithTag("PressStartCircle").GetComponent<Animator>().enabled = true;
+    }
+
+    
+}
---
+++ Assets/TitleUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleUI : MonoBehaviour
+{
+    public void ShowPressStart()
+    {
+       
+        GameObject.FindGameObjectWithTag("PressStartUI").GetComponent<SpriteRenderer>().enabled = true;
+        GameObject.FindGameObjectWithTag("PressStartUI").GetComponent<Animator>().enabled = true;
+    }
+
+    public void PlaySound()
+    {
+        //todo - playsound
+    }
+}
